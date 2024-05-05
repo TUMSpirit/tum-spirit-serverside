b5bf3c529825bc6090068e97f0caf02d30a8ce10
Daniel Günther <d.guenther@tum.de>
2024-05-05 11:17:25 +0200

    add fake auth

--- app/src/routers/language.py
+++ app/src/routers/language.py
@@ -3,9 +3,9 @@ from fastapi import APIRouter
 from ..language_helpers.analyze_chat import analyze_chat
 from ..language_helpers.api.sentiment import get_sentiment
 
-from datetime import datetime
+from ..utils.fakeAuth import CurrentUser
 
-from bson import ObjectId
+from datetime import datetime
 
 router = APIRouter()
 
@@ -13,13 +13,6 @@ router = APIRouter()
 def predict():    
     return analyze_chat()
 
-
-@router.get("/language/hello", tags=["language"])
-def hello_world():
-
-    return {"message": "Hello World"}
-
-
 @router.get("/language/sentiment", tags=["language"])
-def sentiment(startDate:datetime=None, endDate:datetime=None):
-    return get_sentiment(ObjectId("663506d86905198ad715ded8"), startDate, endDate)
+async def sentiment(current_user: CurrentUser, startDate:datetime=None, endDate:datetime=None):
+    return get_sentiment(current_user.id, startDate, endDate)
--- app/src/utils/db.py
+++ app/src/utils/db.py
@@ -1,5 +1,9 @@
 from pymongo import MongoClient
 
+from bson import ObjectId
+from pydantic_core import core_schema
+from typing import Annotated, Any
+
 # Retrieve MongoDB credentials and database info
 MONGO_USER = "root"
 MONGO_PASSWORD = "example"
@@ -17,3 +21,31 @@ db = client[MONGO_DB]
 
 def get_db(collection: str):
     return db[collection]
+
+
+
+class PyObjectId(str):
+    @classmethod
+    def __get_pydantic_core_schema__(
+            cls, _source_type: Any, _handler: Any
+    ) -> core_schema.CoreSchema:
+        return core_schema.json_or_python_schema(
+            json_schema=core_schema.str_schema(),
+            python_schema=core_schema.union_schema([
+                core_schema.is_instance_schema(ObjectId),
+                core_schema.chain_schema([
+                    core_schema.str_schema(),
+                    core_schema.no_info_plain_validator_function(cls.validate),
+                ])
+            ]),
+            serialization=core_schema.plain_serializer_function_ser_schema(
+                lambda x: str(x)
+            ),
+        )
+
+    @classmethod
+    def validate(cls, value) -> ObjectId:
+        if not ObjectId.is_valid(value):
+            raise ValueError("Invalid ObjectId")
+
+        return ObjectId(value)
